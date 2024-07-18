a0c3235bdc23a4a53093e1c9dddc763f30ebd662
Hence <141882882+Hence0x@users.noreply.github.com>
2024-07-18 13:19:15 +0300

    specify min cave size
    allows the user to specify the minimum cave size as a command-line argument

--- main.c
+++ main.c
@@ -1,71 +1,71 @@
 #define _CRT_SECURE_NO_WARNINGS
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
 
-#define MIN_CAVE 50 // min size of code cave to find (bytes)
-
-void fcaves(const char* fname) {
+void fcaves(const char* fname, int min_cave) {
     FILE* f = fopen(fname, "rb");
     if (!f) {
-        printf("cant open file.\n");
+        printf("Can't open file.\n");
         return;
     }
-
     fseek(f, 0, SEEK_END);
     long fsize = ftell(f);
     fseek(f, 0, SEEK_SET);
-
     unsigned char* buf = (unsigned char*)malloc(fsize);
     if (!buf) {
-        printf("mem alloc failed.\n");
+        printf("Memory allocation failed.\n");
         fclose(f);
         return;
     }
-
     fread(buf, 1, fsize, f);
     fclose(f);
 
     PIMAGE_DOS_HEADER dh = (PIMAGE_DOS_HEADER)buf;
     PIMAGE_NT_HEADERS nh = (PIMAGE_NT_HEADERS)(buf + dh->e_lfanew);
+    
 
-    // go through sections
     PIMAGE_SECTION_HEADER sec = IMAGE_FIRST_SECTION(nh);
     for (int i = 0; i < nh->FileHeader.NumberOfSections; i++, sec++) {
         unsigned char* start = buf + sec->PointerToRawData;
         unsigned char* end = start + sec->SizeOfRawData;
         int csize = 0;
-
         for (unsigned char* p = start; p < end; p++) {
             if (*p == 0) {
                 csize++;
             }
             else {
-                if (csize >= MIN_CAVE) {
-                    printf("cave found in %s at 0x%X, size: %d bytes\n",
+                if (csize >= min_cave) {
+                    printf("Cave found in %s at 0x%X, size: %d bytes\n",
                         sec->Name, (unsigned int)(p - buf - csize), csize);
                 }
                 csize = 0;
             }
         }
-
-        // check for cave at section end
-        if (csize >= MIN_CAVE) {
-            printf("cave found in %s at 0x%X, size: %d bytes\n",
+        if (csize >= min_cave) {
+            printf("Cave found in %s at 0x%X, size: %d bytes\n",
                 sec->Name, (unsigned int)(end - buf - csize), csize);
         }
     }
-
     free(buf);
 }
 
+
+
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <PE_file>\n", argv[0]);
+    if (argc != 3) {
+        printf("Usage: %s <PE_file> <min_cave_size>\n", argv[0]);
         return 1;
     }
 
-    fcaves(argv[1]);
+    int min_cave = atoi(argv[2]);
+    if (min_cave <= 0) {
+        printf("Minimum cave size must be a positive integer.\n");
+        return 1;
+    }
+
+    fcaves(argv[1], min_cave);
     return 0;
 }
